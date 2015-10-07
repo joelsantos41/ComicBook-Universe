91711de9d2212716680d2a04e26d826659f3771d
akatriel <michael.c.gabay@gmail.com>
2015-10-07 01:26:16 -0400

    Nav bar on Stan lee page broken.

--- ComicBookUniverse.rb
+++ ComicBookUniverse.rb
@@ -1,5 +1,7 @@
 require "sinatra"
+require "mandrill"
 
+m = Mandrill::API.new
 
 get "/ComicBookUniverse" do
 	erb :ComicBookUniverse
@@ -10,11 +12,28 @@ get "/StanLee" do
 end
 
 get "/manga" do
-	end
+end
 get "/anime" do
 end
 get "/collectibles" do
 end
+get "/contact" do
+	erb :contact
+end
+post "/contact" do
+	message = {
+		:subject=> "#{params[:subject]}",
+		:from_name=> "#{params[:from_name]}",
+		:text=> "Contact page}",
+		:to=> [{:email=> "thisIsNotReal@hotmail.com",
+				:name=> "IT Department"}],
+		:html=> "<html>#{params[:body]}</html>",
+		:from_email=> "#{params[:email]}" 
+	}
+	sending = m.messages.send (message)
+	print sending
+end
+
 
 
 	
